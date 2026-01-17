abac4674f83128133273c02ecc40ce49b6799ddd
medc-0 <moonisontop76@gmail.com>
2026-01-17 16:50:30 +0100

    feat: implement drag and dropping cards

--- index.js
+++ index.js
@@ -11,16 +11,37 @@ for (const card of cards) {
 }
 
 for (const list of lists) {
-    card.addEventListener("dragover",dragOver);
-    card.addEventListener("dragenter",dragEnter);
-    card.addEventListener("dragleave",dragLeave);
-    card.addEventListener("drop",dragDrop);
+    list.addEventListener("dragover",dragOver);
+    list.addEventListener("dragenter",dragEnter);
+    list.addEventListener("dragleave",dragLeave);
+    list.addEventListener("drop",dragDrop);
 }
 
 function dragStart(e) {
-    e.data.transfer.setData("text/plain", this.id);
+    e.dataTransfer.setData("text/plain", this.id);
 }
 
 function dragEnd() {
     console.log("drag ended");
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.classList.add("over");
+}
+
+function dragLeave(e) {
+    this.classList.remove("over");
+}
+
+function dragDrop(e) {
+    const id = e.dataTransfer.getData("text/plain");
+    const card = document.getElementById(id);
+
+    this.appendChild(card);
+    this.classList.remove("over");
 }
