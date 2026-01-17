9d3408f2c44733b56c41ca61135c1aadfbf49970
medc-0 <moonisontop76@gmail.com>
2026-01-17 16:36:36 +0100

    chore: check drag events and implement functions with HTML API

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.info("Website initialized");
 });
+
+const cards = document.querySelectorAll('.card');
+const lists = document.querySelectorAll('.list');
+
+for (const card of cards) {
+    card.addEventListener("dragstart",dragStart);
+    card.addEventListener("dragend",dragEnd);
+}
+
+for (const list of lists) {
+    card.addEventListener("dragover",dragOver);
+    card.addEventListener("dragenter",dragEnter);
+    card.addEventListener("dragleave",dragLeave);
+    card.addEventListener("drop",dragDrop);
+}
+
+function dragStart(e) {
+    e.data.transfer.setData("text/plain", this.id);
+}
+
+function dragEnd() {
+    console.log("drag ended");
+}
